3c39407cf9749fb97853f280e983e381df53b40f
Meenatchi Vijayakumar <rajarajasekar0912@gmail.com>
2025-09-28 03:28:37 +0530

    Communication via WebClient Demo

--- department/src/main/java/com/banking/department/controller/DepartmentController.java
+++ department/src/main/java/com/banking/department/controller/DepartmentController.java
@@ -1,20 +1,20 @@
 package com.banking.department.controller;
 
-import com.banking.department.config.RestTemplateConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class DepartmentController {
 
     @Autowired
-    RestTemplate restTemplate;
+    WebClient webClient;
 
     @GetMapping("/emp")
     public String greetingsFromEmployee(){
-        return restTemplate.getForObject("http://localhost:8084/", String.class);
+        return webClient.get().uri("http://localhost:8084/").retrieve().bodyToMono(String.class).block();
     }
 
     @GetMapping
